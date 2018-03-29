3ed8698bed598cf7e4b73cc7d9c13f2664fcae62
Asheem Mamoowala <asheem.mamoowala@mapbox.com>
2018-03-28 17:42:49 -0700

    [android] Implement CustomLayerHost

--- platform/android/src/example_custom_layer.cpp
+++ platform/android/src/example_custom_layer.cpp
@@ -8,7 +8,7 @@
 static const GLchar * vertexShaderSource = "attribute vec2 a_pos; void main() { gl_Position = vec4(a_pos, 0, 1); }";
 static const GLchar * fragmentShaderSource = "uniform vec4 fill_color; void main() { gl_FragColor = fill_color; }";
 
-class ExampleCustomLayer {
+class ExampleCustomLayer: mbgl::style::CustomLayerHost {
 public:
     ~ExampleCustomLayer() {
         mbgl::Log::Info(mbgl::Event::General, "~ExampleCustomLayer");
@@ -44,7 +44,7 @@ public:
         glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), background, GL_STATIC_DRAW);
     }
 
-    void render() {
+    void render(const mbgl::style::CustomLayerRenderParameters&) {
         mbgl::Log::Info(mbgl::Event::General, "Render");
         glUseProgram(program);
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -55,6 +55,13 @@ public:
         glUniform4fv(fill_color, 1, color);
 
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    }
+
+    void contextLost() {
+    }
+
+    void deinitialize() {
+
     }
 
     GLuint program = 0;
@@ -82,25 +89,6 @@ void JNICALL nativeSetColor(JNIEnv*, jobject, jfloat red, jfloat green, jfloat b
     ExampleCustomLayer::color[3] = alpha;
 }
 
-void nativeInitialize(void *context) {
-    mbgl::Log::Info(mbgl::Event::General, "nativeInitialize");
-    reinterpret_cast<ExampleCustomLayer*>(context)->initialize();
-}
-
-void nativeRender(void *context, const mbgl::style::CustomLayerRenderParameters& /*parameters*/) {
-    mbgl::Log::Info(mbgl::Event::General, "nativeRender");
-    reinterpret_cast<ExampleCustomLayer*>(context)->render();
-}
-
-void nativeContextLost(void */*context*/) {
-    mbgl::Log::Info(mbgl::Event::General, "nativeContextLost");
-}
-
-void nativeDeinitialize(void *context) {
-    mbgl::Log::Info(mbgl::Event::General, "nativeDeinitialize");
-    delete reinterpret_cast<ExampleCustomLayer*>(context);
-}
-
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
     mbgl::Log::Info(mbgl::Event::General, "OnLoad");
     JNIEnv *env = nullptr;
@@ -117,22 +105,6 @@ extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
         env->ExceptionDescribe();
         return JNI_ERR;
     }
-
-    env->SetStaticLongField(customLayerClass,
-        env->GetStaticFieldID(customLayerClass, "InitializeFunction", "J"),
-        reinterpret_cast<jlong>(nativeInitialize));
-
-    env->SetStaticLongField(customLayerClass,
-        env->GetStaticFieldID(customLayerClass, "RenderFunction", "J"),
-        reinterpret_cast<jlong>(nativeRender));
-
-    env->SetStaticLongField(customLayerClass,
-        env->GetStaticFieldID(customLayerClass, "ContextLostFunction", "J"),
-        reinterpret_cast<jlong>(nativeContextLost));
-
-    env->SetStaticLongField(customLayerClass,
-        env->GetStaticFieldID(customLayerClass, "DeinitializeFunction", "J"),
-        reinterpret_cast<jlong>(nativeDeinitialize));
 
     return JNI_VERSION_1_6;
 }
